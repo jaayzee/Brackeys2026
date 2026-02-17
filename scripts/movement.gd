cf292df3ec7bb05d6549a95da14400789e7194c1
John Zhou <itszhoudoh@gmail.com>
2026-02-16 20:19:47 -0800

    camera rotation

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,12 +1,16 @@
 extends CharacterBody3D
 
-@export var speed = 2.0
-@export var jump_velocity = 2.5
+@export var speed = 1.5
+@export var jump_velocity = 2.0
 @export var acceleration = 10.0
 @export var friction = 10.0
+@export var rotation_speed = 90.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var camera_angle = 0.0
 @onready var sprite = $AnimatedSprite3D
+@onready var camera_pivot = $CameraPivot
+@onready var camera = $CameraPivot/Camera3D
 
 # Shooting
 @export var bullet: PackedScene # Can be plates or actually bullets
@@ -20,17 +24,33 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 	
+	# Camera rotation
+	if Input.is_action_pressed("rotate_left"):
+		camera_pivot.rotate_y(deg_to_rad(rotation_speed * delta))
+	if Input.is_action_pressed("rotate_right"):
+		camera_pivot.rotate_y(deg_to_rad(-rotation_speed * delta))
+		
 	# Movement input
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+
+	var cam_basis = camera_pivot.global_transform.basis
+	var cam_forward = -cam_basis.z
+	cam_forward.y = 0
+	cam_forward = cam_forward.normalized()
+	var cam_right = camera.global_transform.basis.x
+	cam_right.y = 0
+	cam_right = cam_right.normalized()
+	
+	# Move relative to camera direction
+	var direction = (cam_forward * -input_dir.y + cam_right * input_dir.x).normalized()
 	
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 		
-		# Flip sprite based on horizontal movement
-		if direction.x != 0:
-			sprite.flip_h = direction.x < 0
+		# Flip sprite based on movement
+		if input_dir.x != 0:
+			sprite.flip_h = input_dir.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
